39f27956a7aa90b9846fab2cb71d37648a456b8f
Said Gadjiev <ackusha_dargo@bk.ru>
2022-12-16 22:18:28 +0300

    Finish task_34

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -6,7 +6,23 @@
 [345, 897, 568, 234] -> 2
 */
 
+//получить размер массива
+
+int CorrectNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    while(! isCorrect)
+    {
+        Console.Write(message);
+        if(int.TryParse(Console.ReadLine(), out result)) isCorrect = true;
+        else Console.WriteLine("Enter correct number");
+    }
+    return result;
+}
+
 //Получить массив рандомными трехзначными числами
+
 
 int[] InitArray(int num)
 {
@@ -22,4 +38,19 @@ int[] InitArray(int num)
     return number;
 }
 
-InitArray(5);
+//Показывает количество четных чисел
+
+void PrintArry(int[] array)
+{
+    Console.WriteLine();
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0) count = count + 1;
+    }
+    Console.WriteLine(count);
+}
+
+int num = CorrectNumber("Enter a number -> ");
+int[] array = InitArray(num);
+PrintArry(array);
